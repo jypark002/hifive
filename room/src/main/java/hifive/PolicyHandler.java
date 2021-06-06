14e796ff284898ce2041a7350d7332da4f9c1bc3
jungyoung <jungyoung02@sk.com>
2021-06-07 00:51:41 +0900

    1. id 삭제 -> roomNumber로 변경 처리 : findById 2. PayCancel 처리 시 로직 추가 if (payCanceled.getPayId() == null) return;

--- room/src/main/java/hifive/PolicyHandler.java
+++ room/src/main/java/hifive/PolicyHandler.java
@@ -8,6 +8,8 @@ import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class PolicyHandler{
     @Autowired RoomRepository roomRepository;
@@ -24,8 +26,20 @@ public class PolicyHandler{
             System.out.println("\n\n##### listener RoomAssign : " + paid.toJson() + "\n\n");
 
             //예약 신청한 방 번호 조회, 퇴실 개념이 없기 때문에 상태 검사 하지 않음
-            Room room = roomRepository.findByRoomNumber(paid.getRoomNumber());
+            Optional<Room> optionalRoom = roomRepository.findById(paid.getRoomNumber());
+//            Room room;
+//            if (optionalRoom.equals(Optional.empty())) {
+//                room = new Room();
+//                room.setUsedCount(1);
+//            }
+//            else {
+//                room = optionalRoom.get();
+//                room.setUsedCount(room.getUsedCount() + 1);
+//            }
+//            Room room = roomRepository.findById(paid.getRoomNumber());
 
+//            Room room = roomRepository.findById(paid.getRoomNumber());
+            Room room = optionalRoom.get();
             room.setRoomStatus("FULL");
             room.setUsedCount(room.getUsedCount() + 1);
             room.setConferenceId(paid.getConferenceId());
@@ -42,14 +56,17 @@ public class PolicyHandler{
     public void wheneverPayCanceled_CancelRoomAssign(@Payload PayCanceled payCanceled){
 
         if(!payCanceled.validate()) {
-            System.out.println("##### listener RoomAssign Fail");
+            System.out.println("##### listener CancelRoomAssign Fail");
             return;
         }
 
         else{
+
+            if (payCanceled.getPayId() == null) return;
+
             //취소 시 조회 - findByPayId
             Room room = roomRepository.findByPayId(payCanceled.getPayId());
-        
+
             //취소 시 방에 등록된 conferenceId, PayId 0으로 초기화 
             room.setRoomStatus("EMPTY");
             room.setUsedCount(room.getUsedCount() - 1);
